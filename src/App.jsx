e8dd959e9722912b455be7f6b1f012d08e093613
Geraldo Gomes <geraldoj8@gmail.com>
2025-04-02 13:22:54 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -60,7 +60,7 @@ function App() {
     };
 
     try {
-      const res = await fetch("http://127.0.0.1:8000/detect_anomaly/", {
+      const res = await fetch("https://be-anomalia.onrender.com/detect_anomaly/", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(requestData),
